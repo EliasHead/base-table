2eefd99776296ec91e8bfcaac724caf3cd160fd3
elias costa <eliashead.costa@gmail.com>
2021-07-15 13:43:06 -0300

    first commit

---
+++ src/components/columns.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+  {
+    Header: 'Id_Treino',
+    accessor: 'id'
+  },
+  {
+    Header: 'Aluno',
+    accessor: 'alunoId'
+  },
+  {
+    Header: 'exercicio',
+    accessor: 'exercicio'
+  },
+  {
+    Header: 'serie',
+    accessor: 'serie'
+  },
+  {
+    Header: 'repeticao',
+    accessor: 'repeticao'
+  },
+]
